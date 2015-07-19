80289a80931c67e7545c9c97a24adf7e8a010862
Nick Holden <nholden@bu.edu>
2015-07-19 18:02:04 -0400

    Make 404 page independent of layout

--- config.rb
+++ config.rb
@@ -15,6 +15,7 @@
 #
 # With no layout
 # page "/path/to/file.html", :layout => false
+page "404.html", layout: false
 #
 # With alternative layout
 # page "/path/to/file.html", :layout => :otherlayout
